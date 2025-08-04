153923a4d761046716610d9324c8973a3013d7a9
Solomon Ador-Dionisio <solomon.adord@gmail.com>
2025-08-04 02:28:21 -0400

    made changes

--- src/app/projectspage/projectspage.ts
+++ src/app/projectspage/projectspage.ts
@@ -15,40 +15,40 @@ export class Projectspage {
       gitlink: "https://github.com/sadsalmonnn/Trees-for-Treemap",
       description: "An interactive file system visualizer, featuring a recursive treemap for intuitive data exploration.",
       tags: ["python", "py os"],
-      imgpath: "/assets/images/projectimgs/treemap.jpeg"
+      imgpath: "./assets/images/projectimgs/treemap.jpeg"
     },
         {
       title: "Drawing Application",
       gitlink: "https://github.com/sadsalmonnn/Digital-Drawing-Application",
       description: "Supports shape drawing, freehand tools, editing features, and file operations.",
       tags: ["java", "javafx", "css", "junit"],
-      imgpath: "/assets/images/projectimgs/drawing-app.jpeg"
+      imgpath: "./assets/images/projectimgs/drawing-app.jpeg"
     },
     {
       title: "Mysh Shell",
       gitlink: "https://github.com/sadsalmonnn/Mysh-Unix-Shell",
       description: "A modular Unix shell and concurrent client-server chat system in C using POSIX APIs and UNIX sockets.",
       tags: ["c", "posix apis", "unix sockets", "bash"],
-      imgpath: "/assets/images/projectimgs/mysh.jpeg"
+      imgpath: "./assets/images/projectimgs/mysh.jpeg"
     },
         {
       title: "Othello Game",
       gitlink: "https://github.com/sadsalmonnn/Othello-Game",
       description: "An OOP Othello game in Java with full game logic, rule enforcement, and modular design.",
       tags: ["python",],
-      imgpath: "/assets/images/projectimgs/othello.jpeg"
+      imgpath: "./assets/images/projectimgs/othello.jpeg"
     }, {
       title: "Simple Portfolio",
       gitlink: "https://github.com/sadsalmonnn/SolomonPortfolio.git",
       description: "A personal portfolio showcasing software projects, built with modern web technologies",
       tags: ["angular", "tailwind", "typescript", "html", "css"],
-      imgpath: "/assets/images/projectimgs/portfolio1.jpeg"
+      imgpath: "./assets/images/projectimgs/portfolio1.jpeg"
     }, {
       title: "Animated Portfolio",
       gitlink: "https://github.com/sadsalmonnn/SolomonPortfolioV2.git",
       description: "A responsive Angular portfolio featuring an animated timeline, project showcase, and dynamic layout.",
       tags: ["angular", "tailwind", "typescript", "html", "css"],
-      imgpath: "/assets/images/projectimgs/portfolio2.jpeg"
+      imgpath: "./assets/images/projectimgs/portfolio2.jpeg"
     }
 ]
 
